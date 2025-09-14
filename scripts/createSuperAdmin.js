ea40e9496a07f85792bd244f9a9b8088076acdbc
Baptiste Martin <81614331+baptiste-mrtn@users.noreply.github.com>
2025-09-14 02:34:35 +0200

    remove sensitive data

--- scripts/createSuperAdmin.js
+++ scripts/createSuperAdmin.js
@@ -4,11 +4,11 @@ import db from "../src/config/db.js";
 async function createSuperAdmin() {
 	try {
 		// Remplis ces informations pour créer le super admin
-		const firstName = "Baptiste";
-		const lastName = "Martin";
-		const email = "baptiste.mrtn@outlook.com";
-		const password = "***REMOVED***"; // 🔒 choisis un bon mot de passe fort
-		const phoneNumber = "***REMOVED***"; // Optionnel, si tu veux l'ajouter
+		const firstName = "";
+		const lastName = "";
+		const email = "";
+		const password = ""; // 🔒 choisis un bon mot de passe fort
+		const phoneNumber = ""; // Optionnel, si tu veux l'ajouter
 
 		// 1️⃣ Hash du mot de passe
 		const hashedPassword = await bcrypt.hash(password, 10);
