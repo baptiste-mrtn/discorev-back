8804ae1f5858e4014f8126e22fafdb218a83d30f
Baptiste Martin <baptiste.martin@le-campus-numerique.fr>
2025-11-25 18:13:11 +0100

    fix: tags routes middleware

--- src/routes/tagRoutes.js
+++ src/routes/tagRoutes.js
@@ -1,16 +1,21 @@
 import express from "express";
 import TagController from "../controllers/tagController.js";
 import authenticateToken from "../middlewares/authMiddleware.js";
-import isAdmin from "../middlewares/isAdmin.js";
+import { isAdmin, isRecruiter } from "../middlewares/roles.js";
 
 const router = express.Router();
 
 router.get("/", TagController.getApprovedOnly); // tous les tags par catégorie
 router.get("/recruiter/:recruiterId", TagController.getByRecruiterId); // tags d’un recruteur
-router.post("/recruiter/:recruiterId", authenticateToken, TagController.setRecruiterTags); // ✅ ici
+router.post(
+	"/recruiter/:recruiterId",
+	authenticateToken,
+	isRecruiter,
+	TagController.setRecruiterTags
+); // ✅ ici
 
 // Admin
-router.get("/admin", TagController.getAllForAdmin);
-router.patch("/:id/approve", TagController.approveTag);
+router.get("/admin", authenticateToken, isAdmin, TagController.getAllForAdmin);
+router.patch("/:id/approve", authenticateToken, isAdmin, TagController.approveTag);
 
 export default router;
